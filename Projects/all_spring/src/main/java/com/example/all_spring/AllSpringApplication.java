f591bf4b3f909a37e1fd5d9dfba6788140197325
wanvidu <wanvidu@gmail.com>
2019-11-28 05:39:05 +0000

    First

---
+++ Projects/all_spring/src/main/java/com/example/all_spring/AllSpringApplication.java
@@ -0,0 +1,13 @@
+package com.example.all_spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AllSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AllSpringApplication.class, args);
+	}
+
+}
